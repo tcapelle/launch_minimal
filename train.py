c1d7f39ba7ec6d3277b2b1f3324084e739611127
Thomas Capelle <tcapelle@pm.me>
2023-04-05 23:31:50 +0200

    train func

---
+++ train.py
@@ -0,0 +1,18 @@
+import wandb
+import numpy as np
+
+PROJECT = "another_launch_project"
+
+default_config = dict(lr=0.001,epochs=10)
+
+def train(config):
+    run = wandb.init(project=PROJECT, config=config)
+    wandb.run.log_code()
+    config = wandb.config
+    for epoch in range(config.epochs):
+        loss = np.random.random()
+        run.log({"loss": loss, "epoch": epoch})
+    run.finish()
+
+if __name__ == "__main__": train(default_config)
+    
