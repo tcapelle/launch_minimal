868e02e7f41218e3ea248bb5ca87f7255d9c3655
Thomas Capelle <tcapelle@pm.me>
2023-04-06 11:15:31 +0200

    trivial launch script

--- train.py
+++ train.py
@@ -2,11 +2,13 @@ import wandb
 import numpy as np
 
 PROJECT = "another_launch_project"
+# ENTITY = "wandb"
+ENTITY = None
 
 default_config = dict(lr=0.001,epochs=10)
 
 def train(config):
-    run = wandb.init(project=PROJECT, config=config)
+    run = wandb.init(project=PROJECT, entity=ENTITY, config=config)
     wandb.run.log_code()
     config = wandb.config
     for epoch in range(config.epochs):
