3049f748ea6cc0bf55df3d1d3d572d3da58232fe
Thomas Capelle <tcapelle@pm.me>
2023-05-15 16:37:26 +0200

    default entity to launch-test

--- train.py
+++ train.py
@@ -1,19 +1,18 @@
-import wandb
-import numpy as np
+import random, wandb
 
 PROJECT = "another_launch_project"
 # ENTITY = "wandb"
-ENTITY = None
+ENTITY = "launch-test"
+# ENTITY = None
 
 default_config = dict(lr=0.001,epochs=10)
 
 def train(config):
     run = wandb.init(project=PROJECT, entity=ENTITY, config=config)
-    wandb.run.log_code()
+    run.log_code()
     config = wandb.config
     for epoch in range(config.epochs):
-        loss = np.random.random()
-        run.log({"loss": loss, "epoch": epoch})
+        run.log({"loss": random.random(), "epoch": epoch})
     run.finish()
 
 if __name__ == "__main__": train(default_config)
